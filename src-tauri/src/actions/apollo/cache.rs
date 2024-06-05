caeb67d7be81d21d8c6787a2ad53f9bd0aae3b83
msho <mayo_s@hotmail.co.uk>
2024-06-05 19:01:52 +0100

    -

--- src-tauri/src/actions/apollo/cache.rs
+++ src-tauri/src/actions/apollo/cache.rs
@@ -1,9 +1,9 @@
 use tauri::{AppHandle, Manager};
 
-use crate::{actions::controllers::Response2, libs::cache::ApolloCache};
+use crate::{actions::controllers::Response, libs::cache::ApolloCache};
 
 
 #[tauri::command]
-pub async fn accounts_in_use(ctx: AppHandle) -> Response2<Vec<String>> {
-  Response2::ok_data(ctx.state::<ApolloCache>().get_all_account_ids().await)
+pub async fn accounts_in_use(ctx: AppHandle) -> Response<Vec<String>> {
+  Response::ok_data(ctx.state::<ApolloCache>().get_all_account_ids().await)
 }
